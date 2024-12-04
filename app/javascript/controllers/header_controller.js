d58be3e1384779ac12951a182792ede90370d0b2
ale364 <acruz643@gmail.com>
2024-12-04 13:04:39 -0600

    Add Devise and styles to header

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -3,18 +3,19 @@ import {enter, leave, toggle} from 'el-transition'
 
 export default class extends Controller {
 
-	static targets = ['dropdown', 'openUserMenu']
+	static targets =['dropdown', 'openUserMenu']
 	connect () {
 		this.openUserMenuTarget.addEventListener("click", (e)=> {
-			console.log(this.dropdownTarget)
+			//console.log("Header clicked")//
+			openDropdown(this.dropdownTarget)
 		})
 	}
 }
 
 function openDropdown(element) {
-	enter(element).then(() => {
+	toggle(element).then(() => {
 		console.log("Enter transition complete")
-	}) 
+	})
 }
 
 function closeDropdown() {
